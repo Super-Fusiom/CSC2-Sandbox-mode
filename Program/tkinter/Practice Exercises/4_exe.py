ae32dfe438f18335cd15a27e1405d4e0d490734a
Paul <paul21479@gmail.com>
2022-03-24 14:56:11 +1300

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- Program/tkinter/Practice Exercises/4_exe.py
+++ Program/tkinter/Practice Exercises/4_exe.py
@@ -8,16 +8,24 @@ def callback():
     print("Your name is: " + val1)
     print("Your password is: " + val2)
 
+label = Label(root, text="Name")
+label.config(justify=("center"))
+
+label2 = Label(root, text="Password")
+label2.config(justify=("center"))
+
+
 entry = ttk.Entry(root, width=30)
 entry2 = ttk.Entry(root, width=20)
-entry.insert(0, 'Please enter your name')
+entry.insert(0, "")
 entry2.config(show='*')
 
-button = ttk.Button(root, text='WOOOOOW', command=callback)
+button = ttk.Button(root, text='Display AWAY', command=callback)
 
 entry.config(justify='center')
-
+label.pack()
 entry.pack()
+label2.pack()
 entry2.pack()
 button.pack()
 
